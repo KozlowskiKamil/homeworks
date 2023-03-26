1991e18d79397f7cf081eaf1cb32c7d647ee0684
KozlowskiKamil <kamil2k8@gmail.com>
2023-03-26 13:08:06 +0200

    przeniesienie metod menu list i add do innych innych klas gotowe do sprawdzenia

--- src/main/java/pl/isa/homeworks/zadanie2/ListMethod.java
+++ src/main/java/pl/isa/homeworks/zadanie2/ListMethod.java
@@ -1,33 +1,49 @@
-/*
 package pl.isa.homeworks.zadanie2;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class ListMethod {
-    CulturalPiecesApp culturalPiecesApp = new CulturalPiecesApp();
 
-    List<CulturalPiece> culturalPiecesss = culturalPiecesApp.
-*/
-/*    private static List<CulturalPiece> getCulturalPieces() {
-        List<CulturalPiece> culturalPieces = new ArrayList<>();
+    static void showDefaultList(List<CulturalPiece> culturalPieces) {
         culturalPieces.add(new Movie("The Shawshank Redemption", "Stephen King"));
         culturalPieces.add(new Movie("Forrest Gump", "Eric Roth"));
         culturalPieces.add(new Painting("Mona Lisa", "Leonardo da Vinci"));
         culturalPieces.add(new Painting("The Persistence of Memory", "Salvador Dalí"));
         culturalPieces.add(new Music("Californication", "Red Hot Chili Peppers"));
         culturalPieces.add(new Music("Lose Yourself", "Eminem"));
-        return culturalPieces;
-    }*//*
 
-
-    private static void addMusic(List<CulturalPiece> culturalPieces) {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        String input2 = scanner.nextLine();
-        culturalPieces.add(new Music(input, input2));
+        for (CulturalPiece piece : culturalPieces) {
+            System.out.println("Title: " + piece.getTitle());
+            System.out.println(piece.getCreatedBy());
+            System.out.println("It can be experienced by: " + String.join(", ", piece.getExperiences()));
+        }
     }
 
+    static void addMusic(List<CulturalPiece> culturalPieces) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the music title");
+        String title = scanner.nextLine();
+        System.out.println("Enter the music author");
+        String author = scanner.nextLine();
+        culturalPieces.add(new Music(title, author));
+    }
+
+    static void addMovie(List<CulturalPiece> culturalPieces) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the movie title");
+        String title = scanner.nextLine();
+        System.out.println("Enter the movie author");
+        String author = scanner.nextLine();
+        culturalPieces.add(new Movie(title, author));
+    }
+
+    static void addPainting(List<CulturalPiece> culturalPieces) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the painting title");
+        String title = scanner.nextLine();
+        System.out.println("Enter the painting author");
+        String author = scanner.nextLine();
+        culturalPieces.add(new Painting(title, author));
+    }
 }
-*/
--- src/main/java/pl/isa/homeworks/zadanie2/Menu.java
+++ src/main/java/pl/isa/homeworks/zadanie2/Menu.java
@@ -1,31 +1,41 @@
-/*
 package pl.isa.homeworks.zadanie2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import static pl.isa.homeworks.zadanie2.ListMethod.*;
 
 public class Menu {
     protected void showMenu() {
 
+        List<CulturalPiece> culturalPieces = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         boolean isContinue = true;
 
         while (isContinue) {
-            System.out.println("Select a menu number");
+            System.out.println("\nSelect a menu number");
             System.out.println("1. Watch masterpieces");
-            System.out.println("2. Add a masterpiece");
-            System.out.println("3. Finish the program");
+            System.out.println("2. Add a masterpiece of music");
+            System.out.println("3. Add a masterpiece of movie");
+            System.out.println("4. Add a masterpiece of painting");
+            System.out.println("5. Finish the program");
 
             if (scanner.hasNextInt()) {
                 int userChoose = scanner.nextInt();
-                if (userChoose > 0 && userChoose < 4) {
-                    if (userChoose == 1) break;
-                    else if (userChoose == 2) {
-                        System.out.println("Podaj tytuł:");
+                if (userChoose > 0 && userChoose < 6) {
+                    if (userChoose == 1) {
+                        showDefaultList(culturalPieces);
+                    } else if (userChoose == 2) {
+                        addMusic(culturalPieces);
                     } else if (userChoose == 3) {
+                        addMovie(culturalPieces);
+                    } else if (userChoose == 4) {
+                        addPainting(culturalPieces);
+                    } else if (userChoose == 5) {
                         System.out.println("Program finished, goodbye");
-                        System.exit(0);
+                        isContinue = false;
                     }
-
                 } else {
                     System.out.println("Wrong number selected");
                     System.exit(0);
@@ -36,13 +46,4 @@ public class Menu {
             }
         }
     }
-
-*/
-/*    private static void addMusic(List<CulturalPiece> culturalPieces) {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        String input2 = scanner.nextLine();
-        culturalPieces.add(new Music(input, input2));
-    }*//*
-
-}*/
+}
